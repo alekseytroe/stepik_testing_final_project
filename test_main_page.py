da59ac2aeb6973bf2fba6c0f9800d4bd4f951bf8
alekseytroe <alekseytroe@gmail.com>
2021-03-22 18:16:33 +0300

    added files from pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,13 @@
 # test_main_page.py::test_guest_can_go_to_login_page
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
